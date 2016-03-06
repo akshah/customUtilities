0402ebbc3d393b0f5d08dcf2e7df15d07f8d0129
Anant Shah <akshah@cs.colostate.edu>
2016-03-06 16:57:31 -0700

    Updates to readme

--- customUtilities/helperFunctions.py
+++ customUtilities/helperFunctions.py
@@ -1,4 +1,12 @@
 import time
+import sys
 
-def current_time():
-    return int(time.time()),time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())
+
+def currentTime():
+    return int(time.time()), time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+
+
+def checkPythonVersion(minRequired=3,minSubRequired=0):
+    if sys.version_info < (minRequired,minSubRequired):
+        print("ERROR: Please use python version {0}.{1}+".format(minRequired,minSubRequired))
+        exit(0)
